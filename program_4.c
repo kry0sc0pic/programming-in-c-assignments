1e536b658ef3de4be2ff832627d3afce30936e09
Krishaay Jois <krishaayjois@gmail.com>
2023-10-16 09:37:47 +0530

    Fixed Matrix Multiplication Code (#4)

--- program_4.c
+++ program_4.c
@@ -1,31 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
+int main() {
 // create an two random 3x3 matrices
-int a[3][3], b[3][3] , c[3][3];
+    int a[3][3], b[3][3], c[3][3] = {{0, 0, 0},
+                                     {0, 0, 0},
+                                     {0, 0, 0}};
 
 // randomize a and b
-for(int i=0;i<3;i++){
-    for(int j=0;j<3;j++){
-        a[i][j] = rand();
-        b[i][j] = rand();
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            a[i][j] = rand();
+            b[i][j] = rand();
+        }
     }
-}
 
-// make C a null matrix
-for(int i=0;i<3;i++) {
-    for (int j = 0; j < 3; j++) {
-        c[i][j] = 0;
+// display a
+    printf("Matrix A\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("%s", "\n");
     }
-}
+
+// display b
+    printf("Matrix B\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", b[i][j]);
+        }
+        printf("%s", "\n");
+    }
 
 // matrix multiplication
-for(int i=0;i<3;i++){
-    for(int j=0;j<3;j++){
-        c[i][j] += a[i][j] * b[j][i];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            c[i][j] = a[i][0] * b[j][0] + a[i][1] * b[j][1] + a[i][2] * b[j][2];
+        }
     }
-}
 
+// display c
+    printf("Matrix C\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("%s", "\n");
 
+    }
 
+    return 0;
 }
