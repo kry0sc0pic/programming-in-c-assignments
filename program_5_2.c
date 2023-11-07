7708921779bc7f3b320f4cf1a461b88f95ccb190
Krishaay Jois <krishaayjois@gmail.com>
2023-11-07 06:27:24 +0000

    5.x

---
+++ program_5_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int num,rem,rev=0;
+	int *pn,*pr;
+	printf(" Enter the number :");
+	scanf("%d",&num);
+
+	pn=&num;
+	pr=&rev;
+	do
+	{
+		rem=(*pn)%10;
+		*pr=(*pr * 10)+rem;
+		*pn=(*pn)/10;
+	}while(*pn>0);	
+	printf(" Reverse of Number is : %d",*pr);
+	return 0;
+	
+}
